fbaeab72a55f748b162d1eaff8bc285e74a256f3
Alice-beauty <115928236+Alice-beauty@users.noreply.github.com>
2022-11-14 16:01:44 +0300

    Add files via upload

---
+++ profile.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   string firstname;
+getline( cin , firstname);
+cout<<"enter first name";
+cout<<firstname;
+
+string lastname;
+getline( cin , lastname);
+cout<<"enter last name";
+cout<<lastname;
+
+string fullname;
+getline( cin , fullname);
+cout<<"enter full name";
+cout<<fullname;
+
+string favoritemeal;
+getline( cin , favoritemeal);
+cout<<"enter favorite meal";
+cout<<favoritemeal;
+
+string favoritemovie;
+getline( cin , favoritemovie);
+cout<<"enter favorite movie";
+cout<<favoritemovie;
+
+return 0;
+}
